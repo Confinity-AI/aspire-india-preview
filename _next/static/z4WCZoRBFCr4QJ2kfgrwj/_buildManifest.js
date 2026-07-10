1225e937681763c355ea8d381917a5bd084aa826
Confinity <ryan@confinity.com>
2026-07-10 20:17:58 +0700

    Deploy

---
+++ _next/static/z4WCZoRBFCr4QJ2kfgrwj/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/aspire-india-preview//_next/:path+",
+        "destination": "/aspire-india-preview/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/z4WCZoRBFCr4QJ2kfgrwj/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fevidence\u002Fread\u002F[slug]","\u002Ffocus\u002F[slug]","\u002Fnews\u002F[slug]","\u002Fpartners\u002F[slug]","\u002Fprograms\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
